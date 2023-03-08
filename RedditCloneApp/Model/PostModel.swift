975279b29d8669819436a5ea8fcb6c06febdae03
SetUpSim <illiastas@ukr.net>
2023-03-08 15:39:23 +0200

    Create post model

---
+++ RedditCloneApp/Model/PostModel.swift
@@ -0,0 +1,20 @@
+//
+//  PostModel.swift
+//  RedditCloneApp
+//
+//  Created by Illia Stasiuk on 08.03.2023.
+//
+
+import Foundation
+
+//class PostModel {
+//    var authorName: String?
+//    var domain: String?
+//    var timePassed: TimeInterval?
+//    var title: String?
+//    var rating: Int?
+//    var commentsNumber: Int?
+//    var imageUrl: String?
+//
+//    init()
+//}
