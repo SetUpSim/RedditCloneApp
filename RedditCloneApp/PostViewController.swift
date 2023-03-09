09a48bee5af0ace8dfc2df79a6bfebd16e676400
SetUpSim <illiastas@ukr.net>
2023-03-09 09:04:11 +0200

    Rename viewController

--- RedditCloneApp/PostViewController.swift
+++ RedditCloneApp/PostViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PostViewController: UIViewController {
     
     
     @IBOutlet weak var postInfoLabel: UILabel!
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        post = PostService.loadPosts(subreddit: "ios", limit: 1)[0]
+        post = PostService.loadPosts(subreddit: "gaming", limit: 1)[0]
         updatePostView()
     }
     
